000eee238ec58be37b8adc468b4692a585baf695
Freddy Morgan-Smith <fred05.ks@gmail.com>
2025-11-14 04:37:45 +0000

    Fixed the white borders around background image, fixed background image responsiveness during window resizing and added dark mode for accessibility.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Navbar from "./Navbar.jsx";
+import { ThemeProvider } from "./ThemeContext.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
 
